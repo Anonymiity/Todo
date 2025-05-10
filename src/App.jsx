76a96cd72e498832e6f11ab857c0dba4faea8bec
Anonymiity <129653820+Anonymiity@users.noreply.github.com>
2025-05-11 00:49:41 +0530

    Update App.jsx
    There was a case problem in the Heaader,jsx file as there was error deploying the app through vercel.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import Header from './components/header.jsx'
+import Header from './components/Header.jsx'
 import Mains from './components/Mains.jsx'
 
 function App() {
